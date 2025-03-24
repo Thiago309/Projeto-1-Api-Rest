cf0d9469c78ddc7ab1f7bcb821b76937cca3ed7c
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2025-03-24 10:45:03 -0300

    Criação de packages de dominio

--- apirest-spring-boot/src/main/java/avanade2025/projeto1/apirest_spring_boot/Application.java
+++ apirest-spring-boot/src/main/java/avanade2025/projeto1/apirest_spring_boot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApirestSpringBootApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApirestSpringBootApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
