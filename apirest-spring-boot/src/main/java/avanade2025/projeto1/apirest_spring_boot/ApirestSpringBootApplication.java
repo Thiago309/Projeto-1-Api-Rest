3ad9a5137e088b886a1a7020a58412c83a66d94d
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2025-03-24 09:25:15 -0300

    Inicio
    Iniciando as atividades.

---
+++ apirest-spring-boot/src/main/java/avanade2025/projeto1/apirest_spring_boot/ApirestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package avanade2025.projeto1.apirest_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestSpringBootApplication.class, args);
+	}
+
+}
---
+++ apirest-spring-boot/src/test/java/avanade2025/projeto1/apirest_spring_boot/ApirestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package avanade2025.projeto1.apirest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
